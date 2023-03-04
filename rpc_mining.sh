1546fa19c3e89f8343e9ff6e1cb04545935b2a39
huynhchinh307 <45554291+huynhchinh307@users.noreply.github.com>
2023-03-04 08:01:24 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qy8twzmdxpeuxqvyz26mmznsqj76d5c0wqay6590rfqxzp3z8vmt6qg8fpdz5 -r 103.82.22.137:103.82.22.137 -p rpc;
+    sleep 5;
+done
