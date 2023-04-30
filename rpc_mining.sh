7cc3f0dc330b854e66231482b8412b78b2525aad
Huỳnh Công Chính <45554291+huynhchinh307@users.noreply.github.com>
2023-04-30 18:46:23 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy8twzmdxpeuxqvyz26mmznsqj76d5c0wqay6590rfqxzp3z8vmt6qg8fpdz5 -r dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qy8twzmdxpeuxqvyz26mmznsqj76d5c0wqay6590rfqxzp3z8vmt6qg8fpdz5 -r 103.166.185.5:10300 -p rpc;
     sleep 5;
 done
